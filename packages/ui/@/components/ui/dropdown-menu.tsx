68ef224c617cd1bbda2cdcf82de95e491c1097ce
djm2k <36978885+djm2k@users.noreply.github.com>
2025-08-12 21:10:49 +1000

    enhance(ui): Use pointer on `DropdownMenuItem`

--- packages/ui/@/components/ui/dropdown-menu.tsx
+++ packages/ui/@/components/ui/dropdown-menu.tsx
@@ -87,7 +87,7 @@ const DropdownMenuItem = React.forwardRef<
     ref={ref}
     className={cn(
       'ui__dropdown-menu-item',
-      'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       inset && 'pl-8',
       className
     )}
